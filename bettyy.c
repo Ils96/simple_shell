1ab678ad6700de8efc3884023ddfe47e7c1a8bb1
ilias <iliasboulaid1@gmail.com>
2024-02-28 14:40:07 -0800

    shell

---
+++ bettyy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_hello - Prints "Hello, Betty!" to standard output.
+ */
+void print_hello(void)
+{
+	printf("Hello, you are Betty!\n");
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	print_hello();
+	return (0);
+}
